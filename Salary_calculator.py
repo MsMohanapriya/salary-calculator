2f5a704d124ab49e7111435c6b4484f14ddd1e32
MOHANAPRIYA T <94276128+MsMohanapriya@users.noreply.github.com>
2023-07-11 21:20:03 +0530

    Add files via upload

---
+++ Salary_calculator.py
@@ -0,0 +1,21 @@
+
+workingHours = []
+salary = 0
+
+for i in range(7):
+    workingHours.append(int(input()))
+    salary += (workingHours[i] * 100)
+
+    if workingHours[i] > 8:
+        salary += (workingHours[i] - 8) * 15
+
+if workingHours[0]:
+    salary += (workingHours[0] * 100) // 2
+
+if workingHours[6]:
+    salary += (workingHours[6] * 100) // 4
+
+if sum(workingHours) > 40:
+    salary += (sum(workingHours) - 40) * 25
+
+print(int(salary))
